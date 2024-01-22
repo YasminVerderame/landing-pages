7534bc4e34db0302857926a4ed47cc617dc5feac
Yasmin Verderame <yasmin.verderame@mutant.com.br>
2024-01-22 15:32:12 -0300

    add landing page 6

---
+++ landing-page6/README.md
@@ -0,0 +1,17 @@
+# Landing Page
+
+Este repositório contém uma landing page responsiva e atraente, desenvolvida utilizando HTML, CSS, Bootstrap e JavaScript. A combinação destas tecnologias proporciona uma experiência visual moderna e interativa para promover seus produtos, serviços ou projetos.
+
+## Visualização Online
+Confira a landing page em ação aqui:
+https://65aeb45ccae1aa39992ee98f--delicate-baklava-b2c6fb.netlify.app/
+
+## Prévia
+Incluímos uma prévia da landing page para que você possa visualizar facilmente o design:
+
+<img src="https://cdn.discordapp.com/attachments/1102647332350206095/1199055478416736336/screencapture-127-0-0-1-5500-index-html-2024-01-22-15_18_03.png?ex=65c1266f&is=65aeb16f&hm=fb85989f184ec38dfe4a2f6e6d480cbf2074a2a94db868e89c9d69ef2ccf469b&" alt="">
+
+
+## Características Principais
+- Bootstrap Integrado: Utilizamos o framework Bootstrap para um design consistente e responsivo.
+- JavaScript Interativo: Elementos interativos adicionados com JavaScript para uma experiência mais dinâmica.
---
+++ landing-page6/js/main.js
@@ -0,0 +1,78 @@
+(function () {
+  window.onscroll = function () {
+    const header_navbar = document.querySelector(".navbar-area");
+    const sticky = header_navbar.offsetTop;
+    const logo = document.querySelector(".navbar-brand img");
+
+    if (window.pageYOffset > sticky) {
+      header_navbar.classList.add("sticky");
+      logo.src = "assets/img/logo/logo-2.svg";
+    } else {
+      header_navbar.classList.remove("sticky");
+      logo.src = "assets/img/logo/logo.svg";
+    }
+
+    const backToTo = document.querySelector(".scroll-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTo.style.display = "flex";
+    } else {
+      backToTo.style.display = "none";
+    }
+  };
+
+  const pageLink = document.querySelectorAll(".page-scroll");
+
+  pageLink.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(elem.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+        offsetTop: 1 - 60,
+      });
+    });
+  });
+
+  function onScroll(event) {
+    const sections = document.querySelectorAll(".page-scroll");
+    const scrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
+
+    for (let i = 0; i < sections.length; i++) {
+      const currLink = sections[i];
+      const val = currLink.getAttribute("href");
+      const refElement = document.querySelector(val);
+      const scrollTopMinus = scrollPos + 73;
+      if (
+        refElement.offsetTop <= scrollTopMinus &&
+        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+      ) {
+        document.querySelector(".page-scroll").classList.remove("active");
+        currLink.classList.add("active");
+      } else {
+        currLink.classList.remove("active");
+      }
+    }
+  }
+
+  window.document.addEventListener("scroll", onScroll);
+
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  document.querySelectorAll(".page-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+  });
+
+  new WOW().init();
+})();
